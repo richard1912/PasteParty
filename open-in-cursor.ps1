b5d568e41f9d99b6eb7a7cca29fce8d681b4e0a4
Richard Ferretti <richard.ferretti@gmail.com>
2025-11-05 16:40:16 +1100

    disabled mouse over animations

--- open-in-cursor.ps1
+++
@@ -1,8 +0,0 @@
-# Open-Cursor.ps1
-param(
-    [string]$Path = (Get-Location)
-)
-
-$cursorPath = "C:\Users\Richard\AppData\Local\Programs\cursor\Cursor.exe"
-
-Start-Process $cursorPath -ArgumentList "`"$Path`""
